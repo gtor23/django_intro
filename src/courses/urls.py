0c1ab92ccd24e5260f0e627b37c289ac54a06a97
jtor23 <68238871+jtor23@users.noreply.github.com>
2021-09-10 16:27:41 -0500

    intro to django

---
+++ src/courses/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from .views import(
+	#MyListView,
+	CourseDeleteView,
+	CourseUpdateView,
+	CourseCreateView,
+	CourseListView,
+	 CourseView,
+	#my_fbv
+
+
+	)
+
+app_name = 'courses'
+urlpatterns = [
+
+	path('', CourseListView.as_view(), name='courses-list'),
+	#path('', my_fbv, name='courses-list')
+	path('create/', CourseCreateView.as_view(), name='courses-create'),
+
+
+
+	path('<int:id>/', CourseView.as_view(), name='courses-detail'),
+
+	path('<int:id>/update/', CourseUpdateView.as_view(), name='courses-update'),
+
+
+	path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
+
+
+]
